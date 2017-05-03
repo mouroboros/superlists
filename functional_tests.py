71fe6f73e06914e53565931784867d7bef107eb3
Mark Lee <mgl@Mac2.local>
2017-05-03 13:25:14 +0100

    unit tests added

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
-import os
+import os, unittest
 
+class NewVisitorTest (unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test')
 
-browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-browser.get('http://localhost:8000')
+        #enter to do list straight away
+        #she types "buy peacock feathers"
+        #hit enter, page updates to show "buy peacock feathers"
+        #still text box for new items
+        #site has generated unique url
+        #url has persistance
+        #satisfied she goes to sleep
 
-assert 'Django' in browser.title
-
+if __name__ == '__main__':
+    unittest.main()
