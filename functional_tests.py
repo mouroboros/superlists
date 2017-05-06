e98b6f22af24637d4a8aaddffcb34ca1b152985d
mgl <m.g.lee@cs.bham.ac.uk>
2017-05-06 10:01:18 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import os, unittest
 
 class NewVisitorTest (unittest.TestCase):
@@ -6,15 +8,33 @@ class NewVisitorTest (unittest.TestCase):
         self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
     def tearDown(self):
         self.browser.quit()
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #enter to do list straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do list'
+            )
+        
         #she types "buy peacock feathers"
-        #hit enter, page updates to show "buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows
+                ))
+        
         #still text box for new items
+        self.fail('Finish the test!')
         #site has generated unique url
         #url has persistance
         #satisfied she goes to sleep
