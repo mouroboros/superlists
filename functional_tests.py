810fe3981f15c2ab3999069d35b3c8fb541ec55c
mgl <m.g.lee@cs.bham.ac.uk>
2017-05-13 12:52:22 +0100

    Found bug that functional test was sending "Buy peacock feathers" to server Refactored functional tests to use self defined method

--- functional_tests.py
+++ functional_tests.py
@@ -8,9 +8,18 @@ class NewVisitorTest (unittest.TestCase):
         self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a cool new to-do app she goes to check out
+        # the webpage
+        
         self.browser.get('http://localhost:8000')
+        
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -24,17 +33,15 @@ class NewVisitorTest (unittest.TestCase):
             )
         
         #she types "buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+        # when she hits enter, the page updates
+        
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        print (table)
-        rows = table.find_elements_by_tag_name('tr')
-        print (rows)
         
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
         
         #still text box for new items
         self.fail('Finish the test!')
