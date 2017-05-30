a89e0642fa85664b21e6c345abeaaa2d5f908d93
mgl <m.g.lee@cs.bham.ac.uk>
2017-05-30 11:37:50 +0100

    added changed unit tests Up next Each List Should Have its Own URL

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -14,22 +14,27 @@ class HomePageTest (TestCase) :
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-         
-        
+       
 
-        
 
-        
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -37,7 +42,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, "Item the second")        
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest (TestCase):
     def test_uses_list_template(self):
@@ -45,8 +52,9 @@ class ListViewTest (TestCase):
         self.assertTemplateUsed(response, 'list.html')
         
     def test_displays_all_list_items(self):
-        Item.objects.create(text='item1')
-        Item.objects.create(text='item2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item1', list=list_)
+        Item.objects.create(text='item2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
