9ee2d860ceafb5b10070aa57fb6a0938a3c5c057
mgl <m.g.lee@cs.bham.ac.uk>
2017-07-18 14:05:23 +0100

    first steps for functional tests for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,4 +113,30 @@ class NewVisitorTest (LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
         #Satisfied they both go to sleep
 
+    #asthetics
+
+    def test_layout_and_styling (self) :
+        #edith goes to home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #she notices that the window is nicely centred
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
+        # She starts a new list and sees the input is nicely centred there too
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
 
